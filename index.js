d971cc56efd80f8cff532012e5c53ac64ee73737
Anoop Elias <anoopelias@gmail.com>
2017-12-22 02:32:35 +0530

    Added user timings and exceptions

--- index.js
+++ index.js
@@ -22,4 +22,34 @@ $(function() {
             });
         }
     });
+
+    // Feature detects Navigation Timing API support.
+    if (window.performance) {
+      // Gets the number of milliseconds since page load
+      // (and rounds the result since the value must be an integer).
+      var timeSincePageLoad = Math.round(performance.now());
+
+      // Sends the timing event to Google Analytics.
+      gtag('event', 'timing_complete', {
+        'name': 'load',
+        'value': timeSincePageLoad,
+        'event_category': 'jQuery loaded'
+      });
+    }
+
+    try {
+      var r = divide(10, 0);
+    } catch(err) {
+      gtag('event', 'exception', {
+        'description': err,
+        'fatal': false
+      });
+    }
+
+    function divide(x, y) {
+      if (y == 0) {
+        throw "Division by zero";
+      }
+      return x/y;
+    }
 });
